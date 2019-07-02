76080f9872d750104d32a4798bb115d629bcce46
PixelsCommander <denis.radin@gmail.com>
2019-07-02 20:30:27 +0200

    Fixes broken aspect ratio

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import 'pepjs';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
